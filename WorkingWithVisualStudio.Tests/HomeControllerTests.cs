3ea3972a8c7f5753c282390f78df4b31b953800f
H S <sekhah@my.lanecc.edu>
2019-11-21 18:41:09 -0800

    Page 182

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -35,6 +35,37 @@ namespace WorkingWithVisualStudio.Tests
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             // Assert
+            Assert.Equal(controller.Repository.Products, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+        class ModelCompleteFakeRepositoryPricesUnder50 : IRepository
+        {
+            public IEnumerable<Product> Products { get; } = new Product[]
+            {
+                new Product {Name = "P1", Price = 5M},
+                new Product {Name = "P2", Price = 48.95M},
+                new Product {Name = "P3", Price = 19.50M},
+                new Product {Name = "P3", Price = 34.95M}
+            };
+
+            public void AddProduct(Product p)
+            {
+                // do nothing - not required for test
+            }
+        }
+
+        [Fact]
+        public void IndexActionModelIsCompletePricesUnder50()
+        {
+            // Arrange
+            var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
+
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            // Assert
             Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
     }
