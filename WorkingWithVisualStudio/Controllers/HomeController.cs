b0e454fa81076e3ac12a78d4022d7a9b0e1bc333
H S <sekhah@my.lanecc.edu>
2019-11-21 16:13:27 -0800

    Add project files.

---
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products.Where((x => x?.Price < 50)));
+    }
+}
