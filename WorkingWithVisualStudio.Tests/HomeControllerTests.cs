b8afdf9bbe75408afc3b57bcd0dd6f62584550ad
H S <sekhah@my.lanecc.edu>
2019-11-21 18:55:57 -0800

    Pages 185, just shy of Moq!

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -10,13 +10,7 @@ namespace WorkingWithVisualStudio.Tests
     {
         class ModelCompleteFakeRepository : IRepository
         {
-            public IEnumerable<Product> Products { get; } = new Product[]
-            {
-                new Product {Name = "P1", Price = 275M},
-                new Product {Name = "P2", Price = 48.95M},
-                new Product {Name = "P3", Price = 19.50M},
-                new Product {Name = "P3", Price = 34.95M}
-            };
+            public IEnumerable<Product> Products { get; set; }
 
             public void AddProduct(Product p)
             {
@@ -24,43 +18,16 @@ namespace WorkingWithVisualStudio.Tests
             }
         }
 
-        [Fact]
-        public void IndexActionModelIsComplete()
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsComplete(Product[] products)
         {
             // Arrange
             var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository();
-
-            // Act
-            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
-
-            // Assert
-            Assert.Equal(controller.Repository.Products, model,
-                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
-        }
-
-        class ModelCompleteFakeRepositoryPricesUnder50 : IRepository
-        {
-            public IEnumerable<Product> Products { get; } = new Product[]
+            controller.Repository = new ModelCompleteFakeRepository
             {
-                new Product {Name = "P1", Price = 5M},
-                new Product {Name = "P2", Price = 48.95M},
-                new Product {Name = "P3", Price = 19.50M},
-                new Product {Name = "P3", Price = 34.95M}
+                Products = products
             };
-
-            public void AddProduct(Product p)
-            {
-                // do nothing - not required for test
-            }
-        }
-
-        [Fact]
-        public void IndexActionModelIsCompletePricesUnder50()
-        {
-            // Arrange
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
